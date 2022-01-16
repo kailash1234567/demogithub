ffbb064a1cfd9fa0868b75a7c3be374af47c467e
Jindagi <Jindagi@192.168.1.110>
2022-01-16 14:30:41 +0530

    commit and push

---
+++ src/test/java/org/testing/demo/Demo.java
@@ -0,0 +1,22 @@
+package org.testing.demo;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Demo {
+public void Demoresult() {
+	
+	String use = "Yes";
+	
+	if (use == "Yes") {
+		int a = 1;
+		int b = 2;
+		int c = a+b;
+		System.out.println(c);
+	}
+	else {
+		
+		System.out.println("No result found in this plate form.....");
+	}
+}
+}
